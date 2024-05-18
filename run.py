2f6853308af170096692db8833c5fc0e5528aa6e
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-05-18 15:51:42 +0000

    update

--- run.py
+++ run.py
@@ -1,7 +1,114 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-n = 0
-while n != 10:
-    n += 1
-    print("Hello, World!")
+import nasdaqdatalink
+
+def get_country_code():
+    line = "-" * 30
+    print("Country Codes:")
+    print(line)
+
+    country_codes = {
+        'sweden': 'SWE',
+        'united states': 'USA',
+        'denmark': 'DNK',
+        'norway': 'NOR',
+        'brazil': 'BRA',
+        'japan': 'JPN',
+        'russia': 'RUS',
+        'south africa': 'ZAF',
+        'turkey': 'TUR',
+        'mexico': 'MEX',
+        'thailand': 'THA',
+    }
+
+    for country in country_codes:
+        print(f"{country.capitalize()}")
+
+    country_name = input("What Country Would You Like To See?\n").lower()
+
+    if country_name in country_codes:
+        return country_codes[country_name]
+    else:
+        print("Country Code not Found")
+
+
+def get_series_id():
+
+    series_ids = {
+    'NY.GDP.MKTP.CD': 'GDP (current US$)',
+    'NY.GDP.PCAP.CD': 'GDP per capita (current US$)',
+    'SP.POP.TOTL': 'Population, total',
+    'NY.GDP.MKTP.KD.ZG': 'GDP growth (annual %)',
+    'EN.ATM.CO2E.KT': 'CO2 emissions (kt)',
+    'IT.NET.USER.ZS': 'Internet users (% of population)',
+    'SE.PRM.TENR': 'Primary completion rate, total (% of relevant age group)',
+    'SP.POP.GROW': 'Population growth (annual %)',
+    'EG.USE.PCAP.KG.OE': 'Energy use (kg of oil equivalent per capita)',
+    'NY.GDP.DEFL.KD.ZG': 'Inflation, GDP deflator (annual %)'
+    }
+
+    n = 0
+
+    for serie in series_ids:
+        n += 1
+        print(f"{n} {serie}")
+
+    series_name = input("What Country Would You Like To See?").lower()
+
+    if series_name in series_ids:
+        return series_ids[series_name]
+    else:
+        print("Country Code not Found")
+
+
+
+def run_program():
+    keepGoing = "y"
+
+    while keepGoing == "y":
+
+        
+        #series_code = get_series_id()
+
+        series_ids = {
+        'NY.GDP.MKTP.CD': 'GDP (current US$)',
+        'NY.GDP.PCAP.CD': 'GDP per capita (current US$)',
+        'SP.POP.TOTL': 'Population, total',
+        'NY.GDP.MKTP.KD.ZG': 'GDP growth (annual %)',
+        'EN.ATM.CO2E.KT': 'CO2 emissions (kt)',
+        'IT.NET.USER.ZS': 'Internet users (% of population)',
+        'SE.PRM.TENR': 'Primary completion rate, total (% of relevant age group)',
+        'SP.POP.GROW': 'Population growth (annual %)',
+        'EG.USE.PCAP.KG.OE': 'Energy use (kg of oil equivalent per capita)',
+        'NY.GDP.DEFL.KD.ZG': 'Inflation, GDP deflator (annual %)'
+        }
+
+        n = 0
+
+        info = list(series_ids.values())      
+
+        for serie in series_ids:
+            n += 1
+            print(f"{n} ID: {serie}        Description: {info[n-1]}")
+
+        series_name = input("What Country Would You Like To See?\n").upper()
+
+        series_code = series_name
+
+        print(f"KODEN R {series_code}")
+        country_code = get_country_code()
+        
+
+        if country_code:
+            api = nasdaqdatalink.ApiConfig.api_key = "qtocsXQd_6id-x16U5Wq"
+
+            table = nasdaqdatalink.get_table('WB/DATA',series_id=series_code, country_code=country_code)
+
+            if table is not None:
+                gdp_values = table['value']
+                print(table)
+            else:
+                print("Failed to retrieve data.")
+
+        keepGoing = input ("Do you want to keep looking? (y/n) ").lower()
+
+
+run_program()
