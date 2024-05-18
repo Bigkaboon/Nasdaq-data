105a21f35617928b23e3ab9260ef4356b09a9bbc
Dexter Törnqvist Saramies <juicebravo1@gmail.com>
2024-05-18 15:46:16 +0000

    aaa

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+n = 0
+while n != 10:
+    n += 1
+    print("Hello, World!")
